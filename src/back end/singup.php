77579547b8adc85fd257df4866ba3add9f8fb625
Bernal <bernaljosecsfn3@gmail.com>
2024-04-05 11:03:40 -0500

    Mensaje de confirmación

---
+++ src/back end/singup.php
@@ -0,0 +1,10 @@
+<?php 
+$fullname=$_POST['fname'];
+$email=$_POST['email'];
+$password=$_POST['passwd'];
+
+echo "Your fullname".$fullname;
+echo "Your fullname".$email;
+echo "Your fullname".$passwd;
+
+?>
